7731f711a4f6a2921464b35791eaf42b9c28cca6
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-01-11 21:28:24 -0700

    got the merchant class and test to be added into the directory

---
+++ lib/merchant.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class Merchant
+  attr_accessor :name, :id
+
+  def initialize(hash_name, hash_id)
+    @name = hash_name[:name].gsub(" ","")
+    @id   = hash_id[:id]
+  end
+
+
+
+
+end
--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,24 +1,35 @@
+# require_relative './../data/sample/merchant_sample'
+require_relative 'merchant'
+require          'csv'
+require          'pry'
 class MerchantRepository
 
   def all
     []
   end
 
-  def find_by_id
+  def load_data
+    contents = CSV.open "merchant_sample.csv", headers: true, header_converters: :symbol
+    contents.each do |row|
+      
+    end
+  end
 
+  def find_by_id
   end
 
   def find_by_name
-
+    []
   end
 
   def find_all_by_name
 
   end
+end
 
-  def load_data
-
-  end
-
-
+if __FILE__ == $PROGRAM_NAME
+mr = MerchantRepository.new
+mr.load_data
+x = mr.find_by_name
+binding.pry
 end
