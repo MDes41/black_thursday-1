7ad04ce53692e2e13426954b1eb335246b33da79
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-01-13 13:47:06 -0700

    fix username and email

--- lib/load_data.rb
+++ lib/load_data.rb
@@ -1,15 +1,14 @@
 
 module LoadData
 
-
-
-
-
-
-
   def load_data(data)
     contents = CSV.open data, headers: true, header_converters: :symbol
     @all = contents.to_a.map {|row| row.to_hash}
   end
+
+
+
+
+
 
 end
