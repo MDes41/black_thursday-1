8dc0b1ab2512c7faf0a93bec60b82b53dc3d5697
Alex <alexxnavarretee@gmail.com>
2016-01-12 22:39:12 -0700

    added more tests to item rep test file

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -96,8 +96,27 @@ class ItemRepositoryTest < Minitest::Test
     #//HEADERS//
     #id,name,description,unit_price,merchant_id,created_at,updated_at
     #//ITEM LINE//
-    #
+    #263395237,510+ RealPush Icon Set,"You&#39;ve got a total socialmedia iconset! Almost every social icon on the planet earth.
+
     item = ir.find_by_id(263395237)
+
+    assert_equal "263395237", item.id
+  end
+
+  def test_edge_that_find_by_id_returns_known_item_even_when_inputted_as_string
+    skip
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+
+    ir   = se.items
+    #//FROM ITEM.CSV FILE//
+    #//HEADERS//
+    #id,name,description,unit_price,merchant_id,created_at,updated_at
+    #//ITEM LINE//
+    #263395237,510+ RealPush Icon Set,"You&#39;ve got a total socialmedia iconset! Almost every social icon on the planet earth.
+    item = ir.find_by_id("263395237")
 
     assert_equal "263395237", item.id
   end
@@ -114,10 +133,100 @@ class ItemRepositoryTest < Minitest::Test
     #//HEADERS//
     #id,name,description,unit_price,merchant_id,created_at,updated_at
     #//ITEM LINE//
-    #
+    #263395237,510+ RealPush Icon Set,"You&#39;ve got a total socialmedia iconset! Almost every social icon on the planet earth.
+
     item = ir.find_by_id(000000000)
 
     assert_equal nil, item
   end
 
+  def test_find_by_name_returns_a_known_item
+    skip
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+
+    ir   = se.items
+    #//FROM ITEM.CSV FILE//
+    #//HEADERS//
+    #id,name,description,unit_price,merchant_id,created_at,updated_at
+    #//ITEM LINE//
+    #263395237,510+ RealPush Icon Set,"You&#39;ve got a total socialmedia iconset! Almost every social icon on the planet earth.
+    item = ir.find_by_name("510+ RealPush Icon Set")
+
+    assert_equal "510+ RealPush Icon Set", item.name
+  end
+
+  def test_find_by_name_returns_nil_for_unknown_item
+    skip
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+
+    ir   = se.items
+    #//FROM ITEM.CSV FILE//
+    #//HEADERS//
+    #id,name,description,unit_price,merchant_id,created_at,updated_at
+    #//ITEM LINE//
+    #263395237,510+ RealPush Icon Set,"You&#39;ve got a total socialmedia iconset! Almost every social icon on the planet earth.
+    item = ir.find_by_name("Turing School")
+
+    assert_equal nil, item
+  end
+
+  def test_edge_that_no_matter_where_spaces_are_placed_find_by_name_returns_known_item
+    skip
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+
+    ir   = se.items
+    #//FROM ITEM.CSV FILE//
+    #//HEADERS//
+    #id,name,description,unit_price,merchant_id,created_at,updated_at
+    #//ITEM LINE//
+    #263395237,510+ RealPush Icon Set,"You&#39;ve got a total socialmedia iconset! Almost every social icon on the planet earth.
+    item = ir.find_by_name("51 0+ R ealP ush Ic on Se t")
+
+    assert_equal "510+ RealPush Icon Set", item.name
+  end
+
+  def test_edge_that_find_by_name_will_find_capitalized_known_item_name
+    skip
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+
+    ir   = se.items
+    #//FROM ITEM.CSV FILE//
+    #//HEADERS//
+    #id,name,description,unit_price,merchant_id,created_at,updated_at
+    #//ITEM LINE//
+    #263395237,510+ RealPush Icon Set,"You&#39;ve got a total socialmedia iconset! Almost every social icon on the planet earth.
+    item = ir.find_by_name("510+ REALPUSH ICON SET")
+
+    assert_equal "510+ RealPush Icon Set", item.name
+  end
+
+  def test_edge_that_find_by_name_will_find_known_item_typed_with_lowercase_and_upcase_letters
+    skip
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+
+    ir   = se.items
+    #//FROM ITEM.CSV FILE//
+    #//HEADERS//
+    #id,name,description,unit_price,merchant_id,created_at,updated_at
+    #//ITEM LINE//
+    #263395237,510+ RealPush Icon Set,"You&#39;ve got a total socialmedia iconset! Almost every social icon on the planet earth.
+    item = ir.find_by_name("510+ ReaLPuSh iCoN SeT")
+
+    assert_equal "510+ RealPush Icon Set", item.name
+  end
 end
